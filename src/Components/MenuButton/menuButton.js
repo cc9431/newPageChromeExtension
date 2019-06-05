9ec31156dcde2f2fe7d8dfc97a97ef34e7018973
Charles Calder <charles.j.calder@gmail.com>
2019-06-05 16:52:43 -0400

    no hover

--- src/Components/MenuButton/menuButton.js
+++ src/Components/MenuButton/menuButton.js
@@ -3,8 +3,8 @@ import './menuButton.css';
 
 const MenuButton = (props) => (
   <img
-    onMouseEnter={() => props.enter()}
-    onMouseLeave={() => props.exit()}
+    // onMouseEnter={() => props.enter()}
+    // onMouseLeave={() => props.exit()}
     onClick={() => props.handleMenuClick()}
     src={`${process.env.PUBLIC_URL}/menu.jpg`}
     className={props.show ? 'menuButton' : 'menuButton menuButtonHover'}
