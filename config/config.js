765d866972509cb39d68ee2dfe76ee6125485107
Charles Calder <charles.j.calder@gmail.com>
2019-11-11 10:56:03 -0500

    fixing chrome inline chuck error

--- config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-console.log(process.env);
-
-module.exports = {
-  openWeatherUrl: process.env.OPEN_WEATHER_URL
-};
--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Wrapper from './wrapper';
-import path from 'path';
-
-console.log(path.resolve(process.cwd(), '.env'));
-
-require('dotenv').config({ path: './../.env' });
-
-console.log(process.env);
 
 ReactDOM.render(<Wrapper />, document.getElementById('root'));
