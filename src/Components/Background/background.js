47b4cfa16a7a441824b3e2bafd4937e38ba72d53
Charles Calder <charles.j.calder@gmail.com>
2020-06-27 12:29:34 -0400

    init styled components updates

--- src/Components/Background/background.js
+++ src/Components/Background/background.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import './background.css';
+import styled from 'styled-components';
 
-const Background = (props) => {
-  return (
-    <div className="background">
-      <div
-        onClick={() => props.handleBackgroundClick()}
-        style={{ backgroundColor: props.selectedColor }}
-        className="background"
-      />
-      {props.children}
-    </div>
-  );
-};
+const Wrapper = styled`
+  user-select: none;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  z-index: -10;
+  transition: background-color 750ms ease;
+`;
+
+const Background = (props) => (
+  <div className="background">
+    <div
+      onClick={() => props.handleBackgroundClick()}
+      style={{ backgroundColor: props.selectedColor }}
+      className="background"
+    />
+    {props.children}
+  </div>
+);
 
 export default Background;
--- src/Components/MenuButton/menuButton.js
+++ src/Components/MenuButton/menuButton.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import './menuButton.css';
+import styled from 'styled-components';
 
-const MenuButton = (props) => (
-  <div onMouseDown={() => props.hideOrShow()}>
-    <img
-      draggable="false"
-      src={`${process.env.PUBLIC_URL}/bars.svg`}
-      className={props.show ? 'menuButton' : 'menuButton menuButtonHover'}
-      alt="menu"
-    />
-  </div>
-);
+const Wrapper = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: invert(1);
+  height: 50px;
+  width: 50px;
+  margin: 30px;
+  transition: all 0.4s ease;
+  opacity: ${(props) => (props.show ? 0.4 : 0)};
+  visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
+  pointer-events: ${(props) => (props.show ? 'auto' : 'none')}
+  :hover {
+    opacity: 0.8;
+  }
+  :active {
+    opacity: 0;
+  }
+`;
+
+const MenuButton = (props) =>
+  <Wrapper
+    onClick={() => props.hideOrShow()}
+    draggable="false"
+    src={`${process.env.PUBLIC_URL}/bars.svg`}
+    className={props.show ? 'menuButton' : 'menuButton menuButtonHover'}
+    alt="menu"
+  />
 
 export default MenuButton;
